8609ec070e703e4a03abe00070194a905062ade9
Balakrishna Avulapati <ba@bavulapati.com>
2026-05-06 16:45:29 +0530

    add proper TypeScript declarations to index.d.ts (#11)

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,8 @@
-declare module 'hyperschema-swift'
+declare class SwiftHyperschema {
+  toCode(): string
+
+  static from(dir: string): SwiftHyperschema
+  static toDisk(hyperschema: SwiftHyperschema, dir?: string | null): void
+}
+
+export = SwiftHyperschema
